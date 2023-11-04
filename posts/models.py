dce9e8225f0239817c2a1b8216773ef883183a4e
Muhammed Recep Hava <ragab5434@gmail.com>
2023-11-04 10:29:00 +0300

    add the data base design of the post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+    Post:
+        - title
+        - content
+        - draft
+        - author
+        - image
+        - tags
+        - category
+        - components
+"""
+
+'''
+    1: HTML Widget
+    2: Validation
+    3: best for DB
+'''
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=20000)
+    draft = models.BooleanField(default=True)
+
+
+    def __str__(self) -> str:
+        return self.title
