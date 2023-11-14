d40f18fcdad055fc148b3d25f56a0ecbce193d41
Muhammed Recep Hava <ragab5434@gmail.com>
2023-11-14 21:54:53 +0300

    added some edits

--- project/settings.py
+++ project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # My Apps
     'posts',
     'taggit',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPH_MODELS = {
+  'all_applications': False,
+  'group_models': True,
+}
