12c456b64a1f0ea5b5f6eaf49e81a7c6983cdd9e
Muhammed Recep Hava <ragab5434@gmail.com>
2023-12-05 01:06:10 +0300

    show relation of author as a nested object

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,15 +1,22 @@
-# from
-
-
 from rest_framework import serializers
 
 from .models import Post
 
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = UserSerializer()
     category = serializers.StringRelatedField()
-    
+
     class Meta:
         model = Post
         fields = '__all__'
